4187e8e871fb73ad5726f75a4cfb4125fd31b5c1
Caio Pivetta <caiopivetta6@gmail.com>
2023-02-01 17:11:55 +0100

    Project Created

---
+++ src/main/java/com/caiopivetta6/BankDataDemoApplication.java
@@ -0,0 +1,13 @@
+package com.caiopivetta6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDataDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDataDemoApplication.class, args);
+	}
+
+}
