46ada05d641f7a88ab740a7e7d488914529c3a49
Caio Pivetta <caiopivetta6@gmail.com>
2023-02-01 17:21:06 +0100

    Account Created

--- src/main/java/com/caiopivetta6/domain/Account.java
+++ src/main/java/com/caiopivetta6/domain/Account.java
@@ -1,5 +1,50 @@
 package com.caiopivetta6.domain;
 
-public class Account {
+import java.util.Objects;
 
+public abstract class Account {
+
+	
+	private Integer id;
+	private double balance;
+	
+	public Account() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
 }
