d59f9d5f34630d12b356ecfa39b8bf29da9cbcc9
Caio Pivetta <caiopivetta6@gmail.com>
2023-02-02 11:41:29 +0100

    Repositories created

--- src/main/java/com/caiopivetta6/BankDataDemoApplication.java
+++ src/main/java/com/caiopivetta6/BankDataDemoApplication.java
@@ -1,8 +1,18 @@
 package com.caiopivetta6;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.caiopivetta6.domain.Address;
+import com.caiopivetta6.domain.City;
+import com.caiopivetta6.domain.State;
+import com.caiopivetta6.repositories.AccountRepository;
+import com.caiopivetta6.repositories.AddressRepository;
+import com.caiopivetta6.repositories.CityRepository;
+import com.caiopivetta6.repositories.ClientRepository;
+import com.caiopivetta6.repositories.StateRepository;
 
 @SpringBootApplication
 public class BankDataDemoApplication implements CommandLineRunner {
@@ -10,10 +20,42 @@ public class BankDataDemoApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(BankDataDemoApplication.class, args);
 	}
-
+	
+	@Autowired
+	private CityRepository cityRepository;
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	@Autowired
+	private AddressRepository addressRepository;
+	
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		
+		State state = new State(null, "Veneto");
+		City city = new City(null, "Verona", state);
+		
+		
+		stateRepository.save(state);
+		cityRepository.save(city);
+		
+		
+		Address address1 = new Address(null, "Corso Porta Nuova", "Borgo Venezia", "72", "31412", city);
+		addressRepository.save(address1);
+		
+		
+		
+		
 		
 	}
 
